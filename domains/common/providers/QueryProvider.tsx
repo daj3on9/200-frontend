d4db611b2f9d593f185210a6d2c90727b019ad7e
daj3on9 <dajung5623@naver.com>
2025-08-18 15:34:40 +0900

    🎉 chore : 프로젝트 초기 세팅

---
+++ domains/common/providers/QueryProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function QueryProvider({ children }: Props) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
