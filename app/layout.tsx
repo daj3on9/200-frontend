ba5545f5beccc3af150aeeefb2cee6d3a6187d1f
daj3on9 <dajung5623@naver.com>
2025-08-25 14:52:22 +0900

    💄 feat : 시스템 레이아웃 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import QueryProvider from '@/domains/common/providers/QueryProvider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'CHERRY',
@@ -28,11 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <header></header>
-        <QueryProvider>{children}</QueryProvider>
+        <div className="layout-container">
+          <QueryProvider>{children}</QueryProvider>
+        </div>
       </body>
     </html>
   );
