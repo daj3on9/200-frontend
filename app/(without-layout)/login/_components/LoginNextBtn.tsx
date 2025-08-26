38b7827705e1ff6df4223ec7367e3754fe9aff0e
김소연 <ksy9252@naver.com>
2025-08-26 16:09:11 +0900

    ✨feat : 다음 버튼 컴포넌트 추가

---
+++ app/(without-layout)/login/_components/LoginNextBtn.tsx
@@ -0,0 +1,20 @@
+import React, { Dispatch, SetStateAction } from 'react';
+interface Props {
+  setStep: Dispatch<SetStateAction<number>>;
+  canProceed: boolean;
+}
+
+export default function LoginNextBtn({ setStep, canProceed }: Props) {
+  return (
+    <div className="absolute bottom-0 left-0 w-full cursor-pointer">
+      <button
+        className={`w-full py-4  text-Static-White text-base font-bold bg-Primary-Normal ${
+          canProceed ? '' : 'opacity-30'
+        }`}
+        onClick={() => setStep((prev) => prev + 1)}
+      >
+        다음
+      </button>
+    </div>
+  );
+}
