ba9477dcb9775cc547e25aa560aef0ad58bb6d73
김소연 <ksy9252@naver.com>
2025-08-26 16:14:37 +0900

    💄 style : 버튼 cursor pointer추가

--- app/(without-layout)/login/_components/LoginNextBtn.tsx
+++ app/(without-layout)/login/_components/LoginNextBtn.tsx
@@ -8,7 +8,7 @@ export default function LoginNextBtn({ setStep, canProceed }: Props) {
   return (
     <div className="absolute bottom-0 left-0 w-full cursor-pointer">
       <button
-        className={`w-full py-4  text-Static-White text-base font-bold bg-Primary-Normal ${
+        className={`w-full py-4  text-Static-White text-base font-bold bg-Primary-Normal cursor-pointer ${
           canProceed ? '' : 'opacity-30'
         }`}
         disabled={!canProceed}
