3c1d2bdb7471bce77b7c6649f8a6d4b4f1f4d4b6
김소연 <ksy9252@naver.com>
2025-08-28 10:23:20 +0900

    💄 style : 공통 헤더 스타일 수정

--- domains/common/components/header.tsx
+++ domains/common/components/header.tsx
@@ -1,6 +1,7 @@
 'use client';
 import LeftArrowIcon from '@/public/icons/leftarrow.svg';
 import HamburgerIcon from '@/public/icons/hamburger.svg';
+import Image from 'next/image';
 
 type HeaderProps = {
   title?: string;
@@ -30,13 +31,23 @@ export default function Header({
             <LeftArrowIcon />
           </button>
         )}
-        {showLogo && <div className="font-extrabold">LOGO</div>}
+        {showLogo && (
+          <div className="font-extrabold flex w-10 h-10 items-center">
+            <Image
+              src="/icons/Logo.svg"
+              alt="체리 로고"
+              width={100}
+              height={100}
+            />
+            Cherry
+          </div>
+        )}
         {title && <h1 className="text-base font-semibold">{title}</h1>}
       </div>
 
       <div className="flex items-center gap-3">
-        {showCart && <HamburgerIcon />}
-        {showHamburger && <HamburgerIcon />}
+        {showCart && <HamburgerIcon className="w-5 h-5" />}
+        {showHamburger && <HamburgerIcon className="w-5 h-5" />}
       </div>
     </header>
   );
