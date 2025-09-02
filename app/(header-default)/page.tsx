487364f68403ff0e27d57196c90639611c767944
daj3on9 <dajung5623@naver.com>
2025-09-02 13:06:06 +0900

    ✨ feat : 홈 페이지 하단 배너 컴포넌트 추가

--- app/(header-default)/page.tsx
+++ app/(header-default)/page.tsx
@@ -1,24 +1,19 @@
-import { useAuthStore } from '@/domains/common/store/authStore';
 import Image from 'next/image';
 import React from 'react';
 import ArrowRightIcon from '@/public/icons/arrow-right.svg';
 import WelcomeBenner from './_components/WelcomeBenner';
 import BannerSwiper from './_components/BannerSwiper';
-import FAQIcon from '@/public/icons/FAQ.svg';
-import CallIcon from '@/public/icons/call.svg';
 import Link from 'next/link';
+import BottomButtons from './_components/BottomButtons';
 
 export default function page() {
-  // 로그인 상태 확인
-  const { isLoggedIn } = useAuthStore.getState();
-
   return (
     <div className="flex flex-col justify-start items-start ">
       <div className="self-stretch flex-1 py-6 flex flex-col justify-between items-center gap-5">
-        <div className="self-stretch px-3.5 flex flex-col justify-start items-start gap-2.5">
+        <div className="self-stretch flex flex-col justify-start items-start gap-2.5">
           <div className="self-stretch inline-flex justify-start items-start">
-            <div className="w-96 p-2 bg-Static-White rounded-xl flex justify-between items-center">
-              <div className="text-center justify-start text-Label-Subnormal title3-sb font-semibold leading-tight">
+            <div className="p-2 bg-Static-White ds-rounded-m flex flex-1 justify-between items-center">
+              <div className="text-center justify-start text-Label-Subnormal title3-sb">
                 체험 및 이용 안내
               </div>
               <Link
@@ -26,28 +21,21 @@ export default function page() {
                 aria-label="체험 및 이용 안내 바로가기"
                 className="w-4 h-4 relative overflow-hidden"
               >
-                <ArrowRightIcon
-                  width={18}
-                  height={18}
-                  className="fill-Fill-30"
-                />
+                <ArrowRightIcon className="w-[18px] h-[18px] fill-Fill-30" />
               </Link>
             </div>
           </div>
         </div>
 
         {/* 환영 배너 */}
-        <div className="self-stretch px-3.5 flex flex-col justify-start items-start">
-          <div className="self-stretch p-3 bg-Static-White rounded-xl inline-flex justify-start items-start">
-            <WelcomeBenner
-              isLoggedIn={isLoggedIn}
-              nickname="Nickname"
-            />
+        <div className="self-stretch flex flex-col justify-start items-start">
+          <div className="self-stretch p-3 ds-rounded-m bg-Static-White inline-flex justify-start items-start">
+            <WelcomeBenner />
           </div>
         </div>
 
         {/* 체험하기 이미지 */}
-        <div className="rounded-xl flex flex-col justify-start items-start overflow-hidden">
+        <div className="ds-rounded-m flex flex-col justify-start items-start overflow-hidden">
           <Image
             className="self-stretch relative"
             src="https://placehold.co/360x300"
@@ -66,37 +54,12 @@ export default function page() {
         </div>
 
         {/* 배너 swiper */}
-        <div className="self-stretch px-3.5 flex flex-col justify-center items-start">
+        <div className="self-stretch ds-rounded-m flex flex-col justify-center items-start">
           <BannerSwiper />
         </div>
 
         {/* 하단 버튼 */}
-        <div className="self-stretch px-3.5 inline-flex justify-start items-center gap-m">
-          <Link
-            href="https://www.notion.so/FAQ-25dced6af65280b2a1c0d52939fc6c6d?source=copy_link"
-            aria-label="FAQ 바로 가기"
-            className="flex-1 self-stretch p-6 bg-Static-White ds-rounded-m flex justify-center items-center gap-m overflow-hidden"
-          >
-            <FAQIcon
-              width={24}
-              height={24}
-              className="fill-Fill-20"
-            />
-            <div className="justify-start body1-sb text-Label-Subnormal">
-              FAQ
-            </div>
-          </Link>
-          <div className="flex-1 self-stretch p-6 bg-Static-White ds-rounded-m flex justify-center items-center gap-m overflow-hidden">
-            <CallIcon
-              width={24}
-              height={24}
-              className="fill-Fill-20"
-            />
-            <div className="justify-start body1-sb text-Label-Subnormal">
-              고객센터
-            </div>
-          </div>
-        </div>
+        <BottomButtons />
       </div>
     </div>
   );
