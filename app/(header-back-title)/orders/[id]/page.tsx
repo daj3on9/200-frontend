b1bda5d75838a0de4ae639016f2743f122cd685f
daj3on9 <dajung5623@naver.com>
2025-09-08 11:27:14 +0900

    🐛 fix : build 오류 수정

--- app/(header-back-title)/orders/[id]/page.tsx
+++ app/(header-back-title)/orders/[id]/page.tsx
@@ -13,8 +13,12 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const order = mockOrders.find((order) => order.orderNumber === id);
 
   if (!order) {
