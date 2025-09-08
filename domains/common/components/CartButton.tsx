78b413402f14b8b8cf66812760ad9a267fd70f05
김소연 <ksy9252@naver.com>
2025-09-08 14:05:36 +0900

    ✨ feat : 장바구니 개수별 헤더 아이콘 변경

--- domains/common/components/CartButton.tsx
+++ domains/common/components/CartButton.tsx
@@ -2,18 +2,37 @@
 
 import { useRouter } from 'next/navigation';
 import BasketIcon from '@/public/icons/Score=0.svg';
+import Basket1Icon from '@/public/icons/Score=1.svg';
+import Basket2Icon from '@/public/icons/Score=2.svg';
+import Basket3Icon from '@/public/icons/Score=3.svg';
 import { useAuthStore } from '../store/authStore';
+import { useCartQuery } from '@/domains/cart/hooks/useCartQuery';
 
 export default function CartButton() {
   const router = useRouter();
   const { isLoggedIn } = useAuthStore.getState();
+  const { cartQuery } = useCartQuery();
 
   const moveToCart = () => {
     if (isLoggedIn) {
       router.push('/cart');
+      return;
     } else {
       router.push('/login');
     }
+  };
+
+  const getBasketIcon = () => {
+    const cartItems = cartQuery.data ?? [];
+    const cartCount = cartItems.length;
+    if (!isLoggedIn) return <BasketIcon className="w-6 h-6 fill-Fill-20" />;
+    if (cartCount === 0) return <BasketIcon className="w-6 h-6 fill-Fill-20" />;
+    if (cartCount === 1)
+      return <Basket1Icon className="w-6 h-6 fill-Fill-20" />;
+    if (cartCount === 2)
+      return <Basket2Icon className="w-6 h-6 fill-Fill-20" />;
+    if (cartCount === 3)
+      return <Basket3Icon className="w-6 h-6 fill-Fill-20" />;
   };
 
   return (
@@ -22,7 +41,7 @@ export default function CartButton() {
       onClick={moveToCart}
       className="cursor-pointer"
     >
-      <BasketIcon className="w-6 h-6 fill-Fill-20" />
+      {getBasketIcon()}
     </button>
   );
 }
