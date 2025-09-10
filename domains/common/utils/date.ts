6fe99d86f33c8117f59983880576cdbeb4242394
김소연 <ksy9252@naver.com>
2025-09-10 10:51:38 +0900

    ✨ feat :  날짜 변환 파일 타입 변경

--- domains/common/utils/date.ts
+++ domains/common/utils/date.ts
@@ -1,8 +1,7 @@
 import dayjs from 'dayjs';
 
-export const formatDate = (dateNumber: number) => {
-  const dateStr = dateNumber.toString();
-  const date = dayjs(dateStr);
+export const formatDate = (dateNumber: string) => {
+  const date = dayjs(dateNumber);
   const days = ['일', '월', '화', '수', '목', '금', '토'];
   const day = days[date.day()];
   return `${date.format('YY.MM.DD')}(${day})`;
